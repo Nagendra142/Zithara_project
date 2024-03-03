9107229d50e4ce890d555ff34523f0c52a633732
nagendra <kallemnagendrababu142@gmail.com>
2024-03-03 12:39:52 +0530

    Adding all files into repo

---
+++ server/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE postgres;
+
+CREATE TABLE sir(
+    id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+); 
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express=require("express");
+const app=express();
+const cors=require('cors');
+const pool=require("./db");
+
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes//
+
+//create a todo
+// app.post("/sirs",async(req,res)=>{
+//     console.log(req.body);
+//     try{
+//         const {description}=req.body;
+        
+//         const newTodo=await pool.query("INSERT INTO sir (description) VALUES($1) RETURNING *",
+//             [description]
+//         );
+//         res.json(newTodo.rows[0]);
+//     }
+//     catch(err){
+//         console.error(err.message);
+//     }
+// })
+
+//get all records
+app.get("/sir",async(req,res)=>{
+    try{
+        const allTodos=await pool.query("SELECT * FROM content");
+        res.status(200).json(allTodos.rows);
+    }
+    catch(err){
+        console.error(err.message)
+    }
+})
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log("server has started on pport 5000")
+});
